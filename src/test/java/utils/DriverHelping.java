b22e683c3c4ea57005bd621cdf91edbb55bc55e2
timapotion <timapotion@gmail.com>
2025-11-12 20:40:27 -0600

    created a new version of driver helper for cross browser testing

---
+++ src/test/java/utils/DriverHelping.java
@@ -0,0 +1,36 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverHelping {
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void initDriver(String browser) {
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver.set(new ChromeDriver());
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver.set(new FirefoxDriver());
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver.set(new EdgeDriver());
+        } else {
+            throw new RuntimeException("Unsupported browser: " + browser);
+        }
+
+        getDriver().manage().window().maximize();
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove(); // cleanup so threads don’t leak
+        }
+    }
+}
