6705e70286e55749337292091399fa4758620866
codewise-nurbek <nabdirasulov0@gmail.com>
2025-11-13 19:03:40 -0600

    Optimized verison of code

--- src/test/java/conflict_practice/Hello.java
+++ src/test/java/conflict_practice/Hello.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.next();
-        System.out.println("Hello, " + name + " how are you doing!");
+        method();
+        greetings();
+    }
 
+    public static void method() {
         for (int i = 0; i < 10; i++) {
             System.out.println("This is feature/conflict branch");
             System.out.println("we will try to merge it !");
         }
-
     }
+
+    public static void greetings() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + " how are you doing!");
+    }
+
+
 }
