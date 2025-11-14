dfd0d20899411fbf5154679820e4266a96b45f06
codewise-nurbek <nabdirasulov0@gmail.com>
2025-11-13 18:24:10 -0600

    Added class for practicing conflics

---
+++ src/test/java/conflict_practice/Hello.java
@@ -0,0 +1,18 @@
+package conflict_practice;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+        System.out.println("Hello here we will practice conflicts");
+    }
+}
