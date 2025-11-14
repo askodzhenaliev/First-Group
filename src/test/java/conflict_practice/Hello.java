87b96f1b99170b3b34ffb2d55db860d031fac3ad
kibadatova-netizen <kibadatova@gmail.com>
2025-11-13 19:39:11 -0600

    create for loop

--- src/test/java/conflict_practice/Hello.java
+++ src/test/java/conflict_practice/Hello.java
@@ -4,33 +4,8 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        method();
-        greetings();
-    }
-
-    public static void method() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("This is feature/conflict branch");
-            System.out.println("we will try to merge it !");
-            System.out.println("we will try to merge it !");
-            System.out.println("we will try to merge it !");
-
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello, how are you?");
         }
     }
-
-    public static void greetings() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.next();
-        System.out.println("Hello, " + name + " how are you doing!");
-    }
-
-    public static void getUserLastname() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your lastname: ");
-        String lastname = scanner.next();
-        System.out.println("your lastname: " + lastname);
-    }
-
-
 }
